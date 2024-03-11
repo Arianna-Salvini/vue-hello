9ac6a2632b584f46942a3c24056693b271ba22f2
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-03-11 13:24:47 +0100

    Add 2 picture with data() of Vue the first from folder and the second from Lorem Picsum

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,11 @@ console.log('Hello Vue');
 const { createApp } = Vue
 
 createApp({
-    data(){
-        return{
-            message : 'Hello Vue!'
+    data() {
+        return {
+            message: 'Hello Vue!',
+            image: './assets/img/01.jpg',
+            imageHttp: 'https://picsum.photos/seed/picsum/500/600'
         }
     }
 }).mount('#app')
